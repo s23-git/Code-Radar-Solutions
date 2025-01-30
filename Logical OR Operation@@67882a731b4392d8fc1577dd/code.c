24b67a4a3e7809f9c78d986d52f100f4fd35ddab
s23-git <shivangi0773.becse24@chitkara.edu.in>
2025-01-30 12:19:57 +0530

    Solution to Logical OR Operation

--- Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
+++ Logical OR Operation@@67882a731b4392d8fc1577dd/code.c
@@ -3,8 +3,8 @@
 int main() {
     int a, b;
 
-    scanf("%d %d", &a &b);
-    if(a > 0 || b < 0){
+    scanf("%d %d", &a, &b);
+    if(a > 0 || b > 0){
         printf("True");
     } else{
         printf("False");
