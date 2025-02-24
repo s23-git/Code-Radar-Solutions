7a58d8c07b0f7c3444156884206a79c4569c75fb
s23-git <shivangi0773.becse24@chitkara.edu.in>
2025-02-24 11:09:11 +0530

    Solution to Check Character Type

---
+++ Check Character Type@@6788dd63490637cfc44e1b95/code.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    char ch;
+    scanf("%c", &ch);
+
+    if(ch is vowel){
+        printf("Vowel");
+    }
+    else if(ch is consonant){
+        printf("Consonant");
+    }else{
+        printf("Digit");
+    }
+    return 0;
+}
